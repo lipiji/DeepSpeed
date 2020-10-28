ab6f369fac99ff7b2326d6bba5f2cd91a6a90ced
Piji Li <pagelee.sd@gmail.com>
2020-10-28 17:14:33 +0800

    Update stage2.py

--- deepspeed/runtime/zero/stage2.py
+++ deepspeed/runtime/zero/stage2.py
@@ -157,8 +157,8 @@ class FP16_DeepSpeedZeroOptimizer(object):
 
         self.cpu_offload = cpu_offload
 
-        self.deepspeed_adam_offload = (cpu_offload
-                                       and type(init_optimizer) == DeepSpeedCPUAdam)
+        self.deepspeed_adam_offload = cpu_offload
+                                       #and type(init_optimizer) == DeepSpeedCPUAdam)
 
         self.device = torch.cuda.current_device() if not self.cpu_offload else 'cpu'
 
